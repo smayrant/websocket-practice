4119928accdd19c163297b46ff0a99cb037bc74d
Sheldrick <mrmayrant@yahoo.com>
2019-05-27 16:33:07 -0400

    added the ability to broadcast a message stating that a user is typing a message when a user is typing

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const io = socket(server);
 io.on("connection", function (socket) {
 	console.log("made socket connection", socket.id);
 
+	// listening for chat event
 	socket.on("chat", function (data) {
 		io.sockets.emit("chat", data);
+	});
+
+	// listening for typing message
+	socket.on("typing", function (data) {
+		socket.broadcast.emit("typing", data);
 	});
 });
--- public/chat.js
+++ public/chat.js
@@ -6,16 +6,27 @@ const message = document.getElementById("message");
 const handle = document.getElementById("handle");
 const btn = document.getElementById("send");
 const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
 
-// emit events
+// Emit events
 btn.addEventListener("click", function () {
 	socket.emit("chat", {
 		message: message.value,
 		handle: handle.value
 	});
+	message.value = "";
 });
 
-// listen for events
+message.addEventListener("keypress", function () {
+	socket.emit("typing", handle.value);
+});
+
+// Listen for events
 socket.on("chat", function (data) {
+	feedback.innerHTML = "";
 	output.innerHTML += "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+	feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
 });
