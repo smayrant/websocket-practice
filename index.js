28db8859969f19e71286fae9af6c9aa2d2201a09
Sheldrick <mrmayrant@yahoo.com>
2019-05-27 15:38:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+const server = app.listen(3000, function () {
+	console.log("listening to requests on port 3000");
+});
+
+// static files
+app.use(express.static("public"));
+
+// socket setup
+const io = socket(server);
+
+io.on("connection", function (socket) {
+	console.log("made socket connection", socket.id);
+});
